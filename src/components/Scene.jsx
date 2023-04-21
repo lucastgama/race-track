00412b9219d1caa57b251d763473a3b4620e5d75
lucastgama <lucas_tgama@hotmail.com>
2023-04-21 15:18:23 -0300

    first commit

---
+++ src/components/Scene.jsx
@@ -0,0 +1,19 @@
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Suspense } from "react";
+import { Track } from "./Track";
+
+export function Scene() {
+  return (
+    <Suspense fallback={null}>
+      <Environment files={"./textures/envmap.hdr"} background={"both"} />
+
+      <Track />
+      <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} />
+      <OrbitControls target={[-2.64, -0.71, 0.03]} />
+    </Suspense>
+  );
+}
