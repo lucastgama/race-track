fa53ecde69f4f9a9c89ee7e6ed2ad2cf228e0b55
lucastgama <lucas_tgama@hotmail.com>
2023-04-22 15:58:06 -0300

    projeto finalizado pt 1

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -6,12 +6,13 @@ import {
 import { Suspense } from "react";
 import { Track } from "./Track";
 import { Ground } from "./Ground";
+import { Car } from "./Car";
 
 export function Scene() {
   return (
     <Suspense fallback={null}>
       <Environment files={"./textures/envmap.hdr"} background={"both"} />
-
+      <Car />
       <Track />
       <Ground />
       <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} />
